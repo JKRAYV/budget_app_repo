33a1e150e074ff7d010dbba2cc38538c05a8d606
nedned1108 <rpmshockwave@gmail.com>
2023-09-12 10:31:38 -0400

    added myuserdetails

--- src/main/java/com/cognixia/group4/filter/JwtRequestFilter.java
+++ src/main/java/com/cognixia/group4/filter/JwtRequestFilter.java
@@ -12,11 +12,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.cognixia.jump.service.MyUserDetailsService;
 import com.cognixia.group4.util.JwtUtil;
 
-public class JwtRequestFilter {
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	private MyUserDetailsService userDetailsService;
