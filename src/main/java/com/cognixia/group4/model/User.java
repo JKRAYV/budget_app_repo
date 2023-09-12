77cc8caf4bde926b61300a3e9b0e5db84f5a3ce5
nedned1108 <rpmshockwave@gmail.com>
2023-09-12 10:56:40 -0400

    updated user

--- src/main/java/com/cognixia/group4/model/User.java
+++ src/main/java/com/cognixia/group4/model/User.java
@@ -4,24 +4,32 @@ import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
 
+import com.cognixia.jump.model.User.Role;
+
 
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	public static enum Role {
+		USER
+	}
 	
 	@Id
 	private Integer id;
 	private String username;
 	private String password;
+	private Role role;
 	
 	public User() {
 		
 	}
 
-	public User(Integer id, String username, String password, Enum role) {
+	public User(Integer id, String username, String password, Role role) {
 		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
+		this.role = role;
 	}
 
 	public Integer getId() {
@@ -47,10 +55,18 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
 	};
 	
 }
