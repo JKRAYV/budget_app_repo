7e2f3716e2221ac533061af33a13a96d6c0b3f58
nedned1108 <rpmshockwave@gmail.com>
2023-09-12 11:09:01 -0400

    updated userrepository

--- src/main/java/com/cognixia/group4/repository/UserRepository.java
+++ src/main/java/com/cognixia/group4/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.cognixia.group4.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
 import com.cognixia.group4.model.User;
 
-public class UserRepository {
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 	
 	public Optional<User> findByUsername(String username);
 	
